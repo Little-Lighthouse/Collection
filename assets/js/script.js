38a94018f3b89ebe7fe4801d1dc9a51167484c77
Little-Lighthouse <139311825+Little-Lighthouse@users.noreply.github.com>
2024-09-21 20:42:57 +0630

    V 0.4.0
    - To Do List #1
    - Other minor updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,43 +1,36 @@
 let username = document.getElementById("username");
 let password = document.getElementById("password");
-localStorage.setItem("Username", "LLH2023");
-localStorage.setItem("Password", "LLH2023");
+localStorage.setItem("Username1", "user");
+localStorage.setItem("Password1", "LLHUser");
+localStorage.setItem("Username2", "admin");
+localStorage.setItem("Password2", "LLHAdmin");
+
 var attempt = 2;
 
 function Login() {
   let InputUserValue = document.getElementById("username").value;
   let InputPasswordValue = document.getElementById("password").value;
 
-  console.log('InputUserValue: ', InputUserValue);
-  console.log('InputPasswordValue: ', InputPasswordValue);
-
-  // Check if username and password values are in localStorage and are not null
-  if (localStorage.getItem("Username") !== null && localStorage.getItem("Password") !== null) {
-    if ((InputUserValue === localStorage.getItem("Username")) &&
-      (InputPasswordValue === localStorage.getItem("Password"))){
-        console.log("Login successful");
-        window.location.replace("user/index.html");
-      }else {
-        attempt --;
-        alert("Access Denied!!!"+" You have left "+attempt+" attempt;");
-      }
-    }
-    /*else {
-      localStorage.clear(); // clear the local storage before adding new values
-      localStorage.setItem("Username", InputUserValue);
-      localStorage.setItem("Password", InputPasswordValue);
-      alert("Signup successful");
-    }*/
-    if( attempt == 0){
-      document.getElementById("username").disabled = true;
-      document.getElementById("password").disabled = true;
-      document.getElementById("button").disabled = true;
-      document.getElementById('username').value = '';
-      document.getElementById('password').value = '';
-      username.style.backgroundColor = "#dddddd";
-      password.style.backgroundColor = "#dddddd";
-    }
+  // Check if the input credentials match either set in localStorage
+  if (InputUserValue === localStorage.getItem("Username1") && InputPasswordValue === localStorage.getItem("Password1")) {
+    window.location.replace("user/index.html");
+  } else if (InputUserValue === localStorage.getItem("Username2") && InputPasswordValue === localStorage.getItem("Password2")) {
+    window.location.replace("admin/index.html");
+  } else {
+    attempt--;
+    alert("Access Denied!!! You have " + attempt + " attempts left.");
   }
+
+  if( attempt == 0){
+    document.getElementById("username").disabled = true;
+    document.getElementById("password").disabled = true;
+    document.getElementById("button").disabled = true;
+    document.getElementById('username').value = '';
+    document.getElementById('password').value = '';
+    username.style.backgroundColor = "#dddddd";
+    password.style.backgroundColor = "#dddddd";
+  }
+}
   
 document.addEventListener("keypress", e => {
   if(e.key === "Enter") {
