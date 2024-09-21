4c2df81039093014cd988bd1e29c280f9075f914
Little-Lighthouse <139311825+Little-Lighthouse@users.noreply.github.com>
2024-09-21 16:37:31 +0630

    V 0.3.0
    - Updated user file
    - Several updates

--- user/script.js
+++ user/script.js
@@ -1,13 +1,21 @@
-let name = document.getElementById("name");
-let department = document.getElementById("dept");
-let term = document.getElementById("term");
-let status = document.getElementById("status");
-
 function loadInfo(){
-    name.innerHTML = "Thomas James Alexander";
-    department.innerHTML = "Content writing";
-    term.innerHTML = "5 months";
-    status.innerHTML = "Resign";
+    let Name = document.getElementById("name");
+    let Department = document.getElementById("dept");
+    let Position = document.getElementById("position");
+    let Term = document.getElementById("term");
+    let Status = document.getElementById("status");
+    
+    let NameM = document.getElementById("nameM");
+    let DepartmentM = document.getElementById("deptM");
+
+    Name.innerHTML = "Thomas James Alexander";
+    Department.innerHTML = "Content writing";
+    Position.innerHTML = "Team Leader";
+    Term.innerHTML = "5 months";
+    Status.innerHTML = "Resign";
+
+    NameM.innerHTML = Name.innerHTML;
+    DepartmentM.innerHTML = Department.innerHTML;
 }
 loadInfo();
 
